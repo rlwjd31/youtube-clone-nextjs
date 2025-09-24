c52c07b2274a678d83026f9e7ca0432dd5b5193e
rlwjd31 <rlwjd31@naver.com>
2025-09-24 22:33:21 +0900

    User Icon Component by shadcn
    - init by shadcn
    - will be use at header section

--- src/app/(youtube-site)/page.tsx
+++ src/app/(youtube-site)/page.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
 
 export default async function Home() {
-
   return (
     <div>
-      <Image src="/untitle.avif" alt="unsplash" width={500} height={500} />
+      home page
     </div>
   );
 }
