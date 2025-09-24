f027ff97cf8039e7baa84fdbfdb4f7f07f9541af
rlwjd31 <rlwjd31@naver.com>
2025-09-24 14:31:18 +0900

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    // contentDispositionType: "attachment",
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    remotePatterns: [
+      { protocol: "https", hostname: "nextjs.org", pathname: "/**" },
+    ],
+  },
+};
 
 export default nextConfig;
