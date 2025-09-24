556f48599962b073582a38fda2a01828cb19c095
rlwjd31 <rlwjd31@naver.com>
2025-09-25 02:04:52 +0900

    Add drawer mode toggle feature
    - take isInDrawer props to toggle drawer
    - take onClickClose from the parent callback

--- src/components/elements/Logo.tsx
+++ src/components/elements/Logo.tsx
@@ -3,9 +3,16 @@
 import IconButton from "@/components/elements/IconButton";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { IoClose } from "react-icons/io5";
 import { RxHamburgerMenu } from "react-icons/rx";
 
-export default function Logo() {
+export default function Logo({
+  isInDrawer = false,
+  onClickClose = () => {},
+}: {
+  isInDrawer?: boolean;
+  onClickClose?: () => void;
+}) {
   const router = useRouter();
 
   const onClickLogo = () => router.push("/");
@@ -13,7 +20,10 @@ export default function Logo() {
 
   return (
     <section className="flex items-center gap-3">
-      <IconButton Icon={RxHamburgerMenu} onClickIcon={onClickMenu} />
+      {isInDrawer && <IconButton Icon={IoClose} onClickIcon={onClickClose} />}
+      {!isInDrawer && (
+        <IconButton Icon={RxHamburgerMenu} onClickIcon={onClickMenu} />
+      )}
       <div className="cursor-pointer" onClick={onClickLogo}>
         <Image src="/main-logo.svg" alt="logo" width={100} height={30}></Image>
       </div>
